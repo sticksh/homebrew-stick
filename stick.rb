a382f548f909e250b28f81cdaabd4cd3e1fd88ed
Yannick Gouez <y@mxh.fr>
2018-04-16 20:40:54 -0400

    Update stick.rb

--- stick.rb
+++ stick.rb
@@ -1,7 +1,7 @@
 class Stick < Formula
   desc "stick.sh command line client"
   homepage "http://stick.sh"
-  url "http://releases.stick.sh:8080/stick-0.1.0.tar.gz"
+  url "http://releases.stick.sh:8080/manager-0.1.0.tar.gz"
   sha256 "e64034d0ec1704ed8830734c4c7a957594056a74578c3f09a8988c1f555ba302"
   
   depends_on "unison"
